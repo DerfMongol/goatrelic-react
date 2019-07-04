bee1624f5ee6950a703cb2da23e6ab03eb544c16
Lee Martin <leefredmart@gmail.com>
2019-07-04 08:39:14 -0400

    heroku

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import createStore from './helpers/createStore'
 
 const app = express()
 
-app.use('/api', proxy('http://salty-thicket-36195.herokuapp.com/', {
+app.use('/api', proxy('https://salty-thicket-36195.herokuapp.com', {
     proxyReqOptDecorator(opts) {
         opts.headers['x-forwarded-host'] = 'localhost:3000'
         return opts
