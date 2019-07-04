584d5d97be944f1aea6dee297dcd0ba24126ac87
Lee Martin <leefredmart@gmail.com>
2019-07-04 14:14:38 -0400

    heroku

--- src/client/actions/user-actions.js
+++ src/client/actions/user-actions.js
@@ -1,9 +1,19 @@
 import { FETCH_CURRENT_USER, POST_SPORT_LIST, FETCH_ALL_USERS } from './types'
-import { get, post }from './functions/data'
+import { get, post } from './functions/data'
 
-export const fetchCurrentUser = () => get(`/profile`, FETCH_CURRENT_USER )
-export const fetchAllUsers = () => get(`/users`, FETCH_ALL_USERS )
-export const postSportList = (sportList) => post('/profile', POST_SPORT_LIST, sportList )
+const config = {
+    withCredentials: true,
+    headers: {
+        'Content-Type': 'application/json',
+    }
+}
+export const fetchCurrentUser = () => async (dispatch, getState, api) => {
+    const res = await api.get('/profile', config)
+    dispatch(receiveTreeData(res.data, FETCH_CURRENT_USER))
+}
+
+export const fetchAllUsers = () => get(`/users`, FETCH_ALL_USERS)
+export const postSportList = (sportList) => post('/profile', POST_SPORT_LIST, sportList)
 
 
 
