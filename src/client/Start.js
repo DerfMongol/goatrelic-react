0518d96e25a22593f80d1bf3c97f7b990c75e319
Lee Martin <leefredmart@gmail.com>
2019-07-06 15:03:00 -0400

    heroku

--- src/client/Start.js
+++ src/client/Start.js
@@ -1,17 +1,37 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { renderRoutes } from 'react-router-config'
+import { connect } from 'react-redux'
 
 import Header from './components/Header'
 import { getNbaAllTime, getNhlAllTime, getPgaAllTime } from './actions/allTime-actions'
 import { getNbaCritic, getNhlCritic, getPgaCritic } from './actions/critic-actions'
 import { fetchCurrentUser, fetchAllUsers } from './actions/user-actions'
 
-const Start = ({ route }) => (
-    <div>
-        <Header />
-        {renderRoutes(route.routes)}
-    </div>
-)
+class Start extends Component {
+    constructor(props) {
+        super(props)
+    }
+
+    componentDidMount() {
+        this.props.getNbaAllTime()
+        this.props.getNhlAllTime()
+        this.props.getPgaAllTime()
+        this.props.getNbaCritic()
+        this.props.getNhlCritic()
+        this.props.getPgaCritic()
+        this.props.fetchAllUsers()
+        this.props.fetchCurrentUser()
+    }
+    render() {
+        return (
+            <div>
+                <Header />
+                {renderRoutes(this.props.route.routes)}
+            </div>
+        )
+    }
+
+}
 
 const loadData = (store) => {
     return Promise.all([
@@ -31,7 +51,16 @@ const loadData = (store) => {
 
 export default {
     loadData,
-    component: Start
+    component: connect(null, {
+        getNbaAllTime,
+        getNhlAllTime,
+        getPgaAllTime,
+        getNbaCritic,
+        getNhlCritic,
+        getPgaCritic,
+        fetchAllUsers,
+        fetchCurrentUser
+    })(Start)
 }
 
 
