9f75090a80cbd62cd4fcf1d6443efd3ca72a4f6e
Lee Martin <leefredmart@gmail.com>
2019-07-07 10:13:34 -0400

    heroku

--- src/client/actions/user-actions.js
+++ src/client/actions/user-actions.js
@@ -1,7 +1,7 @@
 import { FETCH_CURRENT_USER, POST_SPORT_LIST, FETCH_ALL_USERS } from './types'
 import { get, post } from './functions/data'
 
-export const fetchCurrentUser = () => get(`/profile`, FETCH_CURRENT_USER)
+export const fetchCurrentUser = () => get(`/current_user`, FETCH_CURRENT_USER)
 export const fetchAllUsers = () => get(`/users`, FETCH_ALL_USERS)
 export const postSportList = (sportList) => post('/profile', POST_SPORT_LIST, sportList)
 
