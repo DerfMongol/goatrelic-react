d47493159827b31066a80c08b481815f8a9ff9e7
Lee Martin <leefredmart@gmail.com>
2019-07-09 22:03:33 -0400

    extra space

--- webpack.client.prod.js
+++ webpack.client.prod.js
@@ -5,11 +5,18 @@ const baseConfig = require('./webpack.base')
 const config = {
    
     mode: 'production',
-    entry: './src/client/client.js'
+    entry: './src/client/client.js',
+    devtool: 'source-map',
     output: {
         path: path.join(__dirname, 'public'),
-        filename: 'bundle.js'
-    } 
+        filename: 'bundle.js',
+        sourceMapFilename: 'bundle.js.map'
+    },
+    devServer: {
+        contentBase: path.join(__dirname, 'public'),
+        host: 'localhost',
+        port: 3000
+    }
 }
 
 module.exports = merge(baseConfig, config)
